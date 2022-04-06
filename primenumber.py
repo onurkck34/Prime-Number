59466709c14efe665c63359584b9b588d1203081
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-04-06 22:46:48 +0300

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -4,7 +4,7 @@ for x in range(2,sayi):
     if (sayi % x) == 0:
         asalMi = False
         break
-if asalMi:  #default ayari true oldugu icin == True yazmaya gerek yok 
+if asalMi:  
     print("ASAL")
 else:
     print("ASAL DEGIL")
